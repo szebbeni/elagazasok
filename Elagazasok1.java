9588607891a9813075d485da2cb77e21b7735937
szebbeni <szebenyi.benjamindaniel@simonyiszki.org>
2025-02-17 13:50:41 +0100

    Egybeágyazott elágazások

--- Elagazasok1.java
+++ Elagazasok1.java
@@ -37,5 +37,33 @@ public class Elagazasok1 {
         if(egesz % 10 == 0 && egesz % 3 == 0) {
             System.out.printf("A 30 osztható 10-zel és 3-mal maradék nélkül");
         }
+        else {
+            System.out.printf("A 30 nem osztható 10-zel és 3-mal maradék nélkül");
+        }
+        egesz = 27;
+        if(egesz % 10 == 0 || egesz % 3 == 0) {
+            System.out.println("A 27 osztható 10-zel és 3-mal maradék nélkül");
+        }
+        else {
+            System.out.println("A 27 nem osztható 10-zel és 3-mal maradék nélkül");
+
+        }
+        
+        double szazalek = 60.0;
+        if(szazalek >= 80) {
+            System.out.println("Jeles");
+        }
+        else if(szazalek >= 60 && szazalek < 80) {
+            System.out.println("Jó");
+        }
+        else if(szazalek >= 50 && szazalek < 60) {
+            System.out.println("Közepes");
+        }
+        else if(szazalek >= 40 && szazalek < 50) {
+            System.out.println("Elégséges");
+        }
+        else {
+            System.out.println("Elégtelen");
+        }
     }
 }
